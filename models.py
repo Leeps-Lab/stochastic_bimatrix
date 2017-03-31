618c47b20451b08b49aae53572e4c582eea33a0e
James Pettit <etherealmachine@gmail.com>
2017-03-31 11:38:24 -0700

    Fixing up models/views to use redwood extension. Added experiments back in to settings (note that they're still partially broken).

--- models.py
+++ models.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-# <standard imports>
 from __future__ import division
-from otree.db import models
-from otree.constants import BaseConstants
-from otree.models import BaseSubsession, BaseGroup, BasePlayer, Decision
+import random
 
 from otree import widgets
+from otree.db import models
+from otree.constants import BaseConstants
 from otree.common import Currency as c, currency_range
+from otree.models import BaseSubsession, BaseGroup, BasePlayer
 
-import random
-# </standard imports>
+from otree_redwood.models import Decision
 
 doc = """
 Two-by-two game with stochastic transitions between payoff matrices.
