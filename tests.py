933095b67c6e67f402842bd1bf2f9e586c3c1ede
James Pettit <etherealmachine@gmail.com>
2017-06-07 12:16:30 -0700

    Fix test.

--- tests.py
+++ tests.py
@@ -5,8 +5,7 @@ from . import views
 class PlayerBot(Bot):
 
     def play_round(self):
-        if self.player.round_number == 1:
-            yield views.Introduction
+        yield views.Introduction
         yield Submission(views.Decision, {}, check_html=False)
         yield views.Results
 
