3115aee5c4925e4d5d521ef7693829db376cbff1
KellerJordan <kellerjordan1998@gmail.com>
2017-06-17 15:36:33 -0700

    added test, fix for payoff function in stochastic_bimatrix

--- models.py
+++ models.py
@@ -90,46 +90,50 @@ class Player(BasePlayer):
             round=self.round_number,
             group=self.group.id_in_subsession
         )
-        # filter further into only transition and decision events
-        useful_events_over_time = []
-        for event in events_over_time:
-            if event.channel == 'decisions' or event.channel == 'transitions': # why is the not None check needed?
-                useful_events_over_time.append(event)
+        useful_events_over_time = [
+            event for event in events_over_time
+            if event.channel == 'decisions' or event.channel == 'transitions'
+        ]
+        self.payoff = get_payoff(
+            useful_events_over_time,
+            self.id_in_group,
+            self.participant.code,
+            Constants.treatments[self.session.config['treatment']]['payoff_grid']
+        )
 
-        payoff = 0
 
-        treatment = Constants.treatments[self.session.config['treatment']]
-        payoff_grids = treatment['payoff_grid']
+def get_payoff(events_over_time, id_in_group, participant_code, payoff_grids):
+    payoff = 0
 
-        my_state, other_state = initial_decision, initial_decision
-        current_matrix = 0
+    # defaults
+    my_state, other_state = 0.5, 0.5
+    current_matrix = 0
 
-        for i, change in enumerate(useful_events_over_time):
-            if change.value == None: break
+    for i, change in enumerate(events_over_time):
+        if change.value == None: break
 
-            print(change.channel, change.value)
-            
-            if change.channel == 'transitions':
-                current_matrix = change.value
-            elif change.channel == 'decisions':
-                if change.participant == self.participant:
-                    my_state = change.value
-                else:
-                    other_state = change.value
+        print(change.channel, change.value)
 
-            payoff_grid = [payoff[self.id_in_group - 1] for payoff in payoff_grids[current_matrix]]
+        if change.channel == 'transitions':
+            current_matrix = change.value
+        elif change.channel == 'decisions':
+            if change.participant.code == participant_code:
+                my_state = change.value
+            else:
+                other_state = change.value
 
-            cur_payoff = (payoff_grid[0] * my_state * other_state +
-                          payoff_grid[1] * my_state * (1 - other_state) +
-                          payoff_grid[2] * (1 - my_state) * other_state +
-                          payoff_grid[3] * (1 - my_state) * (1 - other_state)) / Constants.period_length
+        payoff_grid = [payoff[id_in_group - 1] for payoff in payoff_grids[current_matrix]]
 
-            next_change_time = useful_events_over_time[i + 1].timestamp
+        cur_payoff = (payoff_grid[0] * my_state * other_state +
+                      payoff_grid[1] * my_state * (1 - other_state) +
+                      payoff_grid[2] * (1 - my_state) * other_state +
+                      payoff_grid[3] * (1 - my_state) * (1 - other_state))
 
-            time_diff = (next_change_time - change.timestamp).total_seconds()
+        next_change_time = events_over_time[i + 1].timestamp
 
-            print('cur_payoff={}, time_diff={}'.format(cur_payoff, time_diff))
-            payoff += time_diff * cur_payoff
+        time_diff = (next_change_time - change.timestamp).total_seconds()
 
-        print('payoff={}'.format(payoff))
-        self.payoff = payoff
+        print('cur_payoff={}, time_diff={}'.format(cur_payoff, time_diff))
+        payoff += time_diff * cur_payoff
+
+    return payoff / Constants.period_length
