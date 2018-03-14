c473c64895499a8f7041f9529f3bb34eb4c8d998
hzcooper <zcooper10@comcast.net>
2018-03-14 00:48:21 -0700

    Improved output table

--- views.py
+++ views.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 from ._builtin import Page, WaitPage
-from .models import Constants, Player, parse_config
+from .models import Constants, Player, Subsession, parse_config
 
 from datetime import timedelta
-
+from operator import concat
+from functools import reduce
 
 def vars_for_all_templates(self):
     payoff_grid = parse_config(self.session.config['config_file'])[self.round_number-1]['payoff_grid']
@@ -45,6 +46,12 @@ class Results(Page):
     def is_displayed(self):
         return self.round_number <= self.group.num_rounds()
 
+def get_config_columns(group):
+    payoffs = group.subsession.payoff_grid()
+    payoffs = reduce(concat, payoffs)
+    config = parse_config(group.session.config['config_file'])
+    role_shuffle = config[group.round_number - 1]['shuffle_role']
+    return payoffs + [role_shuffle]
 
 def get_output_table(events):
     header = [
@@ -52,25 +59,70 @@ def get_output_table(events):
         'subsession_id',
         'id_in_subsession',
         'tick',
-        'p1_mean_strategy',
-        'p2_mean_strategy',
+        'p1_strategy',
+        'p2_strategy',
         'p1_code',
         'p2_code',
         'p_switch',
         'current_matrix',
+        'A_payoff1Aa',
+        'A_payoff1Ab',
+        'A_payoff1Ba',
+        'A_payoff1Bb',
+        'A_payoff2Aa',
+        'A_payoff2Ab',
+        'A_payoff2Ba',
+        'A_payoff2Bb',
+        'B_payoff1Aa',
+        'B_payoff1Ab',
+        'B_payoff1Ba',
+        'B_payoff1Bb',
+        'B_payoff2Aa',
+        'B_payoff2Ab',
+        'B_payoff2Ba',
+        'B_payoff2Bb',
+        'role_shuffle',
     ]
     if not events:
         return [], []
     rows = []
     minT = min(e.timestamp for e in events)
     maxT = max(e.timestamp for e in events)
-    last_p1_mean = float('nan')
-    last_p2_mean = float('nan')
     current_matrix = 0
     p1, p2 = events[0].group.get_players()
     p1_code = p1.participant.code
     p2_code = p2.participant.code
     group = events[0].group
+    config_columns = get_config_columns(group)
+    ticks_per_second = 2
+    p1_decision = float('nan')
+    p2_decision = float('nan')
+    for tick in range((maxT - minT).seconds * ticks_per_second):
+        currT = minT + timedelta(seconds=(tick / ticks_per_second))
+        cur_decision_event = None
+        while events[0].timestamp <= currT:
+            e = events.pop(0)
+            if e.channel == 'group_decisions':
+                cur_decision_event = e
+            elif e.channel == 'current_matrix':
+                current_matrix = e.value
+        if cur_decision_event:
+            p1_decision = cur_decision_event.value[p1_code]
+            p2_decision = cur_decision_event.value[p2_code]
+        rows.append([
+            group.session.code,
+            group.subsession_id,
+            group.id_in_subsession,
+            tick,
+            p1_decision,
+            p2_decision,
+            p1_code,
+            p2_code,
+            group.pswitch(p1_decision,p2_decision),
+            current_matrix,
+        ] + config_columns)
+    return header, rows
+"""
     for tick in range((maxT - minT).seconds):
         currT = minT + timedelta(seconds=tick)
         group_decisions_events = []
@@ -103,8 +155,7 @@ def get_output_table(events):
             current_matrix,
         ])
         last_p1_mean = p1_mean
-        last_p2_mean = p2_mean
-    return header, rows
+        last_p2_mean = p2_mean """
 
 
 page_sequence = [
